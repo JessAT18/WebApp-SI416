97a3488652fe948c04f22c24399c179798a2d3ce
JessicaAquino <jessica.aquino.torrez@gmail.com>
2021-04-29 14:44:08 -0400

    Add project files.

---
+++ AplicacionWeb/capaNegocio/Persona.cs
@@ -0,0 +1,105 @@
+﻿using capaDatos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaNegocio
+{
+    public class Persona : clsConexion
+    {
+        private int ci;
+        private string nombre;
+        private string apellidos;
+        private string sexo;
+        private DateTime fecha_nacimiento;
+        private string telefono;
+
+        public Persona()
+        {
+            ci = 0;
+            nombre = "";
+            apellidos = "";
+            sexo = "";
+            fecha_nacimiento = DateTime.Today.Date;
+            telefono = "";
+        }
+
+        public int Ci
+        {
+            get { return this.ci; }
+            set { this.ci = value; }
+        }
+
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+
+        public string Apellidos
+        {
+            get { return this.apellidos; }
+            set { this.apellidos = value; }
+        }
+
+        public string Sexo
+        {
+            get { return this.sexo; }
+            set { this.sexo = value; }
+        }
+
+        public DateTime Fecha_nacimiento
+        {
+            get { return this.fecha_nacimiento; }
+            set { this.fecha_nacimiento = value; }
+        }
+
+        public string Telefono {
+            get { return this.telefono; } 
+            set { this.telefono = value; } 
+        }
+
+        ///Metodos CRUD
+        public bool guardar()
+        {
+            iniciarSP("guardarPersona");
+            parametroInt(ci, "id");
+            parametroVarchar(nombre, "nom", 30);
+            parametroVarchar(apellidos, "ape", 60);
+            parametroVarchar(sexo, "sex", 1);
+            parametroFecha(fecha_nacimiento, "fec");
+            parametroVarchar(telefono, "tel", 12);
+            if (ejecutarSP() == true) { return true; } else { return false; }
+        }
+
+        public bool modificar()
+        {
+            iniciarSP("modificarPersona");
+            parametroInt(ci, "id");
+            parametroVarchar(nombre, "nom", 30);
+            parametroVarchar(apellidos, "ape", 60);
+            parametroVarchar(sexo, "sex", 1);
+            parametroFecha(fecha_nacimiento, "fec");
+            parametroVarchar(telefono, "tel", 12);
+            if (ejecutarSP() == true) { return true; } else { return false; }
+        }
+
+        public bool eliminar()
+        {
+            iniciarSP("eliminarPersona");
+            parametroInt(ci, "id");
+            if (ejecutarSP() == true) { return true; } else { return false; }
+        }
+
+        public DataTable buscar()
+        {
+            iniciarSP("buscarPersona");
+            parametroVarchar(nombre, "buscar", 30);
+            return mostrarData();
+        }
+
+    }
+}
---
+++ AplicacionWeb/capaPresentacion/frmPersona.aspx.cs
@@ -0,0 +1,88 @@
+﻿using capaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace capaPresentacion
+{
+    public partial class frmPersona : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.mostrar();
+        }
+        protected void mostrar()
+        {
+            Persona person = new Persona();
+            person.Nombre = txtBuscar.Text;
+            gvRegis.DataSource = person.buscar();
+            gvRegis.DataBind();
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Persona person = new Persona();
+            person.Ci = Convert.ToInt32(txtCi.Text);
+            person.Nombre = txtNombre.Text;
+            person.Apellidos = txtApellidos.Text;
+            person.Sexo = rb1.Checked ? "M" : "F";
+            person.Fecha_nacimiento = Convert.ToDateTime(txtFecha.Text);
+            person.Telefono = txtTelefono.Text;
+            txtResp.Text = person.guardar() ? "Registro Guardado!" : "Error al registrar";
+            this.mostrar();
+        }
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            this.mostrar();
+        }
+
+        protected void gvRegis_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            rb1.Checked = false;
+            rb2.Checked = false;
+            txtCi.Text = gvRegis.SelectedRow.Cells[0].Text;
+            txtNombre.Text = gvRegis.SelectedRow.Cells[1].Text;
+            txtApellidos.Text = gvRegis.SelectedRow.Cells[2].Text;
+            if (gvRegis.SelectedRow.Cells[3].Text == "M") { rb1.Checked = true; } else { rb2.Checked = true; }
+            DateTime dt = Convert.ToDateTime(gvRegis.SelectedRow.Cells[4].Text);
+            txtFecha.Text = String.Format("{0:yyyy-MM-dd}", dt);
+            txtTelefono.Text = gvRegis.SelectedRow.Cells[5].Text;
+        }
+
+        protected void btnModificar_Click(object sender, EventArgs e)
+        {
+            Persona person = new Persona();
+            person.Ci = Convert.ToInt32(txtCi.Text);
+            person.Nombre = txtNombre.Text;
+            person.Apellidos = txtApellidos.Text;
+            if (rb1.Checked) { person.Sexo = "M"; } else { person.Sexo = "F"; }
+            person.Fecha_nacimiento = Convert.ToDateTime(txtFecha.Text);
+            person.Telefono = txtTelefono.Text;
+            if (person.modificar()) { txtResp.Text = "Registro Modificado..!"; } else { txtResp.Text = "Error al Modificar"; }
+            this.mostrar();
+        }
+
+        protected void btnNuevo_Click(object sender, EventArgs e)
+        {
+            txtCi.Text = "";
+            txtNombre.Text = "";
+            txtApellidos.Text = "";
+            rb1.Checked = false;
+            rb2.Checked = false;
+            txtFecha.Text = "";
+            txtTelefono.Text = "";
+            txtResp.Text = "";
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+            Persona person = new Persona();
+            person.Ci = Convert.ToInt32(txtCi.Text);
+            if (person.eliminar()) { txtResp.Text = "Registro Eliminado..!"; } else { txtResp.Text = "Error al Eliminar"; }
+            this.mostrar();
+        }
+    }
+}
